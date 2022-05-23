8f8043babebbd2082bb1f7eb44de3e91ff57da1c
cybervoigt <cybervoigt@gmail.com>
2022-05-23 10:35:51 -0300

    First commit...

---
+++ index1.js
@@ -0,0 +1,29 @@
+
+
+
+const a = 10
+
+let b = a * 20
+
+console.log(b)
+
+
+const firstName = 'Ricardo'
+const lastName = 'Voigt'
+const fullName = `
+-------
+Name: ${firstName} ${lastName}
+------
+`
+
+console.log(fullName)
+
+
+const objeto1 = {
+    name1: firstName,
+    name2: lastName
+}
+const field = 'name2';
+
+console.log(objeto1.name1, objeto1[field])
+
---
+++ loops.js
@@ -0,0 +1,9 @@
+
+// simple loop...
+for(let i=0; i<10; i++)
+{
+    console.log('i=',i)
+}
+
+
+
