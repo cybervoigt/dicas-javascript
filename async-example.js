0335167885e4dae5fd4125cb620f2eb063911294
cybervoigt <cybervoigt@gmail.com>
2022-05-27 16:27:11 -0300

    more async examples...

--- async-example.js
+++ async-example.js
@@ -7,10 +7,52 @@
 const fs = require ('fs')
 
 
+/**
+ * some functions...
+ * 
+ */
+
+const functionTest = () => {
+    return 'hello...I am a line between the lines 4 and 5.'
+}
+
+const readFilePromise = filepath => {
+
+    // example of a Promise... "I Promise a result!"
+    return new Promise( function(resolve,reject) {
+        fs.readFile(filepath, (err,content) => {
+            resolve(content)
+        })
+    } )
+}
+
+
+const asyncProcessing = async () => {
+
+    try{
+        console.log('============ reading README.md')
+        const content = await readFilePromise('./README.md')
+        return content
+    }catch(err){
+        console.log('My Error:',err)
+
+    }
+}
+
+
+
+
+
+/**
+ *  now starts the output...
+ */
+
+
 console.log('line 1')
 
 // slow line...
 const content = fs.readFile('./calculator.js', (err,content)=>{
+    // here it's a callback function...
     console.log('===================reading calculator.js')
     console.log(String(content))
 })
@@ -19,17 +61,11 @@ const content = fs.readFile('./calculator.js', (err,content)=>{
 console.log('line 2')
 
 
-const readFilePromise = filepath => {
-    return new Promise( (resolve,reject) => {
-        fs.readFile(filepath, (err,content) => {
-            resolve(content)
-        })
-    } )
-}
 
 // slow line...
-readFilePromise('./index1.js')
-    .then( content => {
+const myPromise = readFilePromise('./index1.js')
+
+myPromise.then( content => {
         console.log('=================reading index1.js')
         console.log(String(content))
     } )
@@ -37,6 +73,12 @@ readFilePromise('./index1.js')
 
 console.log('line 3')
 
+console.log(functionTest())
+
+
 console.log('line 4')
 
 
+asyncProcessing().then( content=>console.log(String(content)) )
+
+console.log('line 5')
